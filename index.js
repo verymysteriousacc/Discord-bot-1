60cba80d989a639295a1cf05f6596bce8ec8bd82
verymysteriousacc <ihsanodabas29@gmail.com>
2026-05-12 23:59:34 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === "!ping") {
+    message.channel.send("Pong!");
+  }
+});
+
+client.login(process.env.TOKEN);
